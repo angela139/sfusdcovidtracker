adb537fc505117c7e682f9ec9651ffe8225e8977
Angela Hu <angelahu925@gmail.com>
2021-08-22 00:17:08 -0700

    Fixed merge error

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import requests
 from bs4 import BeautifulSoup
-<<<<<<< HEAD
 import datetime
 import csv
 
@@ -24,29 +23,3 @@ print(table_data)
 with open("SFUSD_Covid_Cases.csv", 'a', newline='') as obj:
     csv_writer = csv.writer(obj)
     csv_writer.writerow(table_data)
-
-
-
-=======
-
-URL = "https://www.sfusd.edu/covid-19-response-updates-and-resources/health-and-safety-guidelines/when-someone-gets-sick"
-page = requests.get(URL)
-soup = BeautifulSoup(page.text, 'html.parser')
-overall_reported = soup.find_all("table")[1]
-headers = []
-header_search = overall_reported.find_all("th")
-for header in header_search:
-    headers.append(header.text)
-
-print(headers)
-
-table_data = []
-
-for items in overall_reported.find_all("tr"):
-    row = {}
-    for td, th in zip(items.find_all("td"), headers):
-        row[th] = td.text.replace('\n', '').strip()
-    table_data.append(row)
-
-print(table_data)
->>>>>>> origin
