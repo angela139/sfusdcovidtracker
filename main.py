7d611825be5a1201b047e104f201992967cfdf30
Angela Hu <angelahu925@gmail.com>
2021-09-02 00:13:02 -0700

    Created html table from data and styled with Bootstrap

--- main.py
+++ main.py
@@ -2,24 +2,39 @@ import requests
 from bs4 import BeautifulSoup
 import datetime
 import csv
+import re
 
 URL = "https://www.sfusd.edu/covid-19-response-updates-and-resources/health-and-safety-guidelines/when-someone-gets-sick/covid-19-testing-dashboard"
 page = requests.get(URL)
 soup = BeautifulSoup(page.text, 'html.parser')
 overall_reported = soup.find_all("table")[0]
+dict_list = []
 
-date_updated = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
-table_data = [str(date_updated)]
+today_date = datetime.datetime.now().strftime("%Y-%m-%d")
+
+web_date = soup.find_all("em")[0].get_text()
+date_list = web_date[14:].replace("@", "").replace(",", "").split()
+month = datetime.datetime.strptime(date_list[0], '%B').month
+if int(date_list[1]) < 10:
+    date_for_compare = date_list[2] + "-" + "0" + str(month) + "-0" + date_list[1]
+    formatted_date = date_list[2] + "-" + "0" + str(month) + "-0" + date_list[1] \
+                     + " " + date_list[3]
+else:
+    date_for_compare = date_list[2] + "-" + "0" + str(month) + "-" + date_list[1]
+    formatted_date = date_list[2] + "-" + "0" + str(month) + "-" + date_list[1] \
+                     + " " + date_list[3]
+
+table_data = [formatted_date]
 
 for rows in overall_reported.find_all("tr")[1:]:
     num = rows.find_all("td")
-    table_data.append(num[1].get_text())
+    case_num = re.sub("[^0-9]", "", num[1].get_text())
+    table_data.append(case_num)
 
-positivity_rate = "{:.3%}".format(int(table_data[4]) / 62800)
-table_data.append(positivity_rate)
-print(table_data)
-
-
-with open("SFUSD_Covid_Cases.csv", 'a', newline='') as obj:
-    csv_writer = csv.writer(obj)
-    csv_writer.writerow(table_data)
+if today_date == date_for_compare:
+    with open("SFUSD_Covid_Cases.csv", 'a') as obj:
+        csv_writer = csv.writer(obj)
+        csv_writer.writerow(table_data)
+    print("Data updated")
+else:
+    print("Different. Data not updated yet for today.")
--- web.py
+++ web.py
@@ -1,12 +1,36 @@
 from flask import Flask
+from flask import render_template
 import pandas as pd
+import csv
+
+dict_list = []
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello world!'
+def index():
+    with open('SFUSD_Covid_Cases.csv') as csv_file:
+        data = csv.reader(csv_file)
+        reverse_data = list(reversed(list(data)))
+
+    count = len(list(reverse_data))
+    if len(dict_list) > 0:
+        pass
+    else:
+        for row in range(count):
+            if row != (count - 1):
+                dict_list.append({
+                    "time": reverse_data[row][0],
+                    "staff": reverse_data[row][1],
+                    "student": reverse_data[row][2],
+                    "color": reverse_data[row][3],
+                    "total": reverse_data[row][4]
+                })
+            else:
+                print("Last row")
+
+    return render_template("index.html", dict_list=dict_list)
 
 
 if __name__ == "__main__":
