b3c235ba5b8663edd132ea6e672014b225e5e07d
Angela Hu <angelahu925@gmail.com>
2021-09-06 23:24:51 -0700

    Changed military time to 12hr format and small changes to graph

--- scraper.py
+++ scraper.py
@@ -5,6 +5,7 @@ import csv
 import re
 import schedule
 import time
+import pandas as pd
 
 URL = "https://www.sfusd.edu/covid-19-response-updates-and-resources/health-and-safety-guidelines/when-someone-gets-sick/covid-19-testing-dashboard"
 
@@ -16,27 +17,12 @@ def job():
     overall_reported = soup.find_all("table")[0]
     dict_list = []
 
-    # Today's Date
-    today_date = datetime.datetime.now().strftime("%Y-%m-%d")
-
     # Date the website updated with new data
     web_date = soup.find_all("em")[0].get_text()
 
     # Array with month, day, time
-    date_list = web_date[14:].replace("@", "").replace(",", "").split()
-
-    # Number version of month from website
-    month = datetime.datetime.strptime(date_list[0], '%B').month
-    # Formatted dates with single digit day
-    if int(date_list[1]) < 10:
-        date_for_compare = date_list[2] + "-" + "0" + str(month) + "-0" + date_list[1]
-        formatted_date = date_list[2] + "-" + "0" + str(month) + "-0" + date_list[1] \
-                         + " " + date_list[3]
-    # Formatted dates with double digit day
-    else:
-        date_for_compare = date_list[2] + "-" + "0" + str(month) + "-" + date_list[1]
-        formatted_date = date_list[2] + "-" + "0" + str(month) + "-" + date_list[1] \
-                         + " " + date_list[3]
+    updated_date = web_date[14:].replace("@", "").replace(",", "")
+    formatted_date = datetime.datetime.strptime(updated_date, '%B %d %Y  %H:%M').strftime('%m-%d-%Y %I:%M %p')
 
     # Array for table data, first index is date
     table_data = [formatted_date]
@@ -61,7 +47,7 @@ def job():
     table_data.append(str(new_cases))
 
     # Putting new data into csv if data was updated
-    if last_row_array[0][:10] != date_for_compare:
+    if last_row_array[0] != formatted_date:
         with open("SFUSD_Covid_Cases.csv", 'a') as obj:
             csv_writer = csv.writer(obj)
             csv_writer.writerow(table_data)
