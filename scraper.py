ad3e22fabdd9730068b833b03ca78344eebcc6b8
Angela Hu <angelahu925@gmail.com>
2021-09-20 23:09:08 -0700

    Added school specific data in csv file

--- scraper.py
+++ scraper.py
@@ -15,7 +15,7 @@ def job():
     page = requests.get(URL)
     soup = BeautifulSoup(page.text, 'html.parser')
     overall_reported = soup.find_all("table")[0]
-    dict_list = []
+    school_reported = soup.find_all("table")[2]
 
     # Date the website updated with new data
     web_date = soup.find_all("em")[0].get_text()
@@ -24,14 +24,20 @@ def job():
     updated_date = web_date[14:].replace("@", "").replace(",", "")
     formatted_date = datetime.datetime.strptime(updated_date, '%B %d %Y  %H:%M').strftime('%m-%d-%Y %I:%M %p')
 
-    # Array for table data, first index is date
-    table_data = [formatted_date]
+    # Array for overall cases table data, first index is date
+    overall_table_data = [formatted_date]
+    schools_table_data = [formatted_date]
 
     # Adds data from table on website to array
     for rows in overall_reported.find_all("tr")[1:]:
         num = rows.find_all("td")
         case_num = re.sub("[^0-9]", "", num[1].get_text())
-        table_data.append(case_num)
+        overall_table_data.append(case_num)
+
+    for rows in school_reported.find_all("tr")[2:6]:
+        num = rows.find_all("td")
+        case_num = re.sub("[^0-9]", "", num[1].get_text())
+        schools_table_data.append(case_num)
 
     # Opens csv to get previous data to calculate # of new cases
     with open("SFUSD_Covid_Cases.csv", "r") as csv_file:
@@ -41,16 +47,19 @@ def job():
         last_row_array = last_row.split(',')
 
     # Number of new cases
-    new_cases = int(table_data[4]) - int(last_row_array[4])
+    new_cases = int(overall_table_data[4]) - int(last_row_array[4])
 
-    # Add number of new cases to table_data array
-    table_data.append(str(new_cases))
+    # Add number of new cases to overall_table_data array
+    overall_table_data.append(str(new_cases))
 
     # Putting new data into csv if data was updated
     if last_row_array[0] != formatted_date:
         with open("SFUSD_Covid_Cases.csv", 'a') as obj:
             csv_writer = csv.writer(obj)
-            csv_writer.writerow(table_data)
+            csv_writer.writerow(overall_table_data)
+        with open("SFUSD_Schools_Cases.csv", 'a') as obj:
+            csv_writer = csv.writer(obj)
+            csv_writer.writerow(schools_table_data)
         print("Data updated.")
     else:
         print("Data not updated yet for today.")
